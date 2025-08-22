70915dc58af1edf041489ef833d4404da030fed2
Ne'matov Saidakbar <Saidakbarnematov2509@gamil.com>
2025-08-23 00:36:34 +0500

    update downloade rapi

--- main.py
+++ main.py
@@ -8,15 +8,22 @@ app = FastAPI()
 @app.get("/api")
 def download_info(url: str = Query(..., description="Video URL kerak")):
     try:
-        # yt-dlp ni JSON formatda ishga tushirish
         command = ["/var/www/downloader-api/venv/bin/yt-dlp", "-j", url]
 
         result = subprocess.run(command, capture_output=True, text=True)
 
         if result.stdout:
-            data = json.loads(result.stdout)
+            # Har bir qatorni alohida JSON sifatida o‘qiymiz
+            data = [json.loads(line) for line in result.stdout.splitlines() if line.strip()]
             return JSONResponse(content=data)
         else:
-            return JSONResponse(content={"error": "Video yuklab bo‘lmadi"}, status_code=500)
+            return JSONResponse(
+                content={
+                    "error": "Video yuklab bo‘lmadi",
+                    "stderr": result.stderr  # ❗ log ham qaytadi
+                },
+                status_code=500
+            )
+
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
